8db5884000f86fc90337978a777d1e865a638340
holczur <holcz.norbert@gmail.com>
2022-10-14 12:36:37 +0200

    D1: Data types, variables, operators, boolean logic, if statement

---
+++ challenges/chl-script.js
@@ -0,0 +1,126 @@
+/*
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK � 
+*/
+const heightMark1 = 1.69;
+const massMark1 = 78;
+
+const heightMark2 = 1.88;
+const massMark2 = 95;
+
+const heightJohn1 = 1.95;
+const massJohn1 = 92;
+
+const heightJohn2 = 1.76;
+const massJohn2 = 85;
+
+//BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+const bmiM1 = massMark1 / heightMark1 ** 2;
+const bmiM2 = massMark2 / heightMark2 ** 2;
+const bmiJ1 = massJohn1 / heightJohn1 ** 2;
+const bmiJ2 = massJohn2 / heightJohn2 ** 2;
+console.log(bmiM1);
+
+const markHigherBMI1 = bmiM1 > bmiJ1;
+const markHigherBMI2 = bmiM2 > bmiJ2;
+
+console.log(markHigherBMI1, markHigherBMI2);
+
+//----------------------------------------------------------------------
+/*
+Coding Challenge #2
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement �
+GOOD LUCK � 
+*/
+
+if (bmiM1 > bmiJ1) {
+  console.log(`Mark's BMI (${bmiM1}) is higher than John's (${bmiJ1})`);
+} else {
+  console.log(`John's BMI (${bmiJ1}) is higher than Mark's (${bmiM1})`);
+}
+
+//----------------------------------------------------------------------
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK �
+*/
+
+const avgDolphins = (96 + 108 + 89) / 3;
+const avgKoalas = (88 + 91 + 110) / 3;
+
+const avgDolphins2 = (97 + 112 + 101) / 3;
+const avgKoalas2 = (109 + 95 + 123) / 3;
+
+const avgDolphins3 = (97 + 100 + 101) / 3;
+const avgKoalas3 = (105 + 95 + 98) / 3;
+
+if (avgDolphins === avgKoalas) {
+  console.log("It's a draw!");
+} else if (avgDolphins > avgKoalas) {
+  console.log('Dolphins won!');
+} else {
+  console.log('Koalas won!');
+}
+
+if (avgDolphins2 === avgKoalas2) {
+  console.log("It's a draw!");
+} else if (avgDolphins2 > avgKoalas2 && avgDolphins2 >= 100) {
+  console.log('Dolphins won!');
+} else {
+  console.log('Koalas won!');
+}
+
+if (avgDolphins2 === avgKoalas2 && avgDolphins3 + avgKoalas3 >= 200) {
+  console.log("It's a draw!");
+} else if (avgDolphins3 > avgKoalas2 && avgDolphins3 >= 100) {
+  console.log('Dolphins won!');
+} else if (avgDolphins3 < avgKoalas2 && avgKoalas3 >= 100) {
+  console.log('Koalas won!');
+} else {
+  console.log('No team has reached the minimum score');
+}
+
+console.log(avgKoalas, avgDolphins);
+console.log(avgKoalas2, avgDolphins2);
+console.log(avgKoalas3, avgDolphins3);
---
+++ study/fundamentals/script.js
@@ -0,0 +1,166 @@
+//---------------- DATA TYPES ---------------------
+/* There are 7 data types:
+    - Number
+    - String
+    - Boolian
+    - Null
+    - Undefined
+    - Symbol (ES2015)
+    - BigInt
+
+    We can declare variables to store them
+    for example:
+    */
+
+let myNumber = 12233;
+let myString = 'this is a string';
+let myBoolean = true;
+let year; //undefined
+let myBigInt = 1e45;
+
+//Variables can be declared several ways
+let modifyMe = 100;
+modifyMe = 200; //reassign
+console.log(modifyMe);
+
+const leaveMealone = 'You cannot change me!';
+/*
+leaveMealone = 'Are you sure';
+console.log(leaveMealone); --> TypeError
+
+Allways use 'const' if you now the variable should'nt change
+and only use 'let' to create variable that you want to change later
+*/
+
+var globalVar =
+  "doesn't matter where did you declare me, I'am allways in the global scope";
+
+//---------------- MATH OPERATORS ---------------------
+modifyMe = modifyMe + 200;
+console.log(modifyMe); //400
+
+modifyMe += 200;
+console.log(modifyMe); //600
+
+modifyMe *= 2;
+console.log(modifyMe); //1200
+
+modifyMe++;
+console.log(modifyMe); //1201
+
+//addition sign can be used to concatenate strings
+const firstName = 'Norbert';
+const lastName = 'Holcz';
+console.log(firstName + ' ' + lastName);
+
+//---------------- COMPARISON OPERATORS ---------------------
+console.log(modifyMe > 1202); //false
+console.log(modifyMe > 1201); //false
+console.log(modifyMe >= 1201); //true
+
+const ageSarah = 21;
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge); //true
+
+/* Js calculates both values first, then it makes the comparison
+check the operator precedence at here: https://www.w3schools.com/js/js_precedence.asp
+*/
+
+const now = 2022;
+console.log(now - 1991 > now - 2018); //true
+
+//---------------- STRING LITERALS ---------------------
+const job = 'programmer';
+const birthYear = 1986;
+const age = now - birthYear;
+
+const norbert = `I'm ${firstName}, a ${age} years old \n ${job}`; // \n for new line
+console.log(norbert);
+
+//---------------- TYPE COVERSION AND COERCION ---------------------
+
+const inputYear = '1991';
+console.log(inputYear + 19); //199119
+console.log(Number(inputYear) + 19); //2010
+console.log(Number('Norbert')); //NaN
+
+//coercion is made automatically
+console.log('23' / '2'); // 11.5
+console.log('23' * '2'); // 46
+console.log('23' + '2' + 5); //with addition it becomes a concatenation --> '2325'
+
+let n = '1' + 1; // '11'
+n = n - 1; // 10
+
+//---------------- IF ELSE STATEMENTS ---------------------
+
+if (age >= 40) {
+  console.log('You are old enough');
+} else {
+  const yearsLeft = 40 - age;
+  console.log(`You are too young. Wait another ${yearsLeft} years 🤡`);
+}
+
+let century;
+if (birthYear <= 2000) {
+  century = 20; //variables declared inside an 'if block' will be unavailable outside the the block
+} else {
+  century = 21;
+}
+
+console.log(century);
+
+// Truthy and falsy values ---------------------
+/*
+    Falsy:
+        0;
+        '';
+        undefined;
+        null;
+        NaN
+    */
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all!");
+} else {
+  console.log('You should have a job');
+}
+
+// Equality
+
+const loose = '18';
+
+if (loose == 18) {
+  console.log('coerced value is accepted');
+}
+if (loose === 18) {
+  console.log('coerced value is accepted');
+} else {
+  console.log('"===" means a strict equaliti: type also has to match');
+} // HINT: allways use strict equality
+
+const favoriteNum = Number(prompt("What's your favorite number?"));
+if (favoriteNum === 23) {
+  console.log('Cool, my number is also 23!');
+} else if (favoriteNum < 23) {
+  console.log('My number is bigger than yours');
+} else {
+  console.log('My number is smaller than yours');
+}
+
+if (favoriteNum !== 23) {
+  console.log('Why not 23?');
+}
+
+// Boolean logic
+const hasDriversLicense = true;
+const hasGoodVision = true;
+const isTired = true;
+
+console.log(hasDriversLicense && hasGoodVision); //true
+console.log(hasDriversLicense || hasGoodVision); //true
+console.log(!hasDriversLicense); //false
+
+const shouldDrive = hasDriversLicense && hasGoodVision && !isTired;
+console.log(shouldDrive); //false

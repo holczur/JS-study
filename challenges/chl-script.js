b9a29d4a0543a143671828366b0bc4418d0db46a
holczur <holcz.norbert@gmail.com>
2022-10-14 15:53:14 +0200

    trenary operator
    Quick challenge of using the trenary operator

--- challenges/chl-script.js
+++ challenges/chl-script.js
@@ -124,3 +124,34 @@ if (avgDolphins2 === avgKoalas2 && avgDolphins3 + avgKoalas3 >= 200) {
 console.log(avgKoalas, avgDolphins);
 console.log(avgKoalas2, avgDolphins2);
 console.log(avgKoalas3, avgDolphins3);
+
+/*
+Coding Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 �
+GOOD LUCK �
+*/
+
+const bill1 = 275;
+const bill2 = 40;
+const bill3 = 430;
+const tip = bill3 >= 50 && bill3 <= 300 ? bill3 * 0.15 : bill3 * 0.2;
+console.log(
+  `The bill was ${bill3}, the tip was ${tip} and the total value is ${
+    bill1 + tip
+  }.`
+);

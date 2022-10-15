cb38444f5ec7d8aeebe4081e251b25a1a7498dd1
holczur <holcz.norbert@gmail.com>
2022-10-15 12:35:38 +0200

    functions
    declaration, expression, arrow

--- study/fundamentals/script.js
+++ study/fundamentals/script.js
@@ -239,3 +239,33 @@ const appleJuice = fruitProcessor(3, 5); //to capture returned value from the fu
 console.log(appleJuice);
 
 console.log(fruitProcessor(1, 3)); //to simply use it's returned value
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2022 - birthYear;
+}
+age = calcAge1(1976);
+console.log(age);
+
+//Function expression
+const calcAge2 = function (birthYear) {
+  return 2022 - birthYear;
+};
+
+let age2 = calcAge2(1991);
+console.log(age2);
+
+//Arrow function
+const calcAge3 = birthYear => 2022 - birthYear; // oneliner that automatically returns value
+let age3 = calcAge3(1972);
+console.log(age3);
+
+const yearsUntilRetirement = birthYear => {
+  // need {} for code block
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+
+let retirement = yearsUntilRetirement(1986);
+console.log(`You have to wait another ${retirement} years until retirement`);

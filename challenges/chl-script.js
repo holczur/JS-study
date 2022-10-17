a15d19e471aaa97ed03228cdc7e13cf53fe6399f
holczur <115772321+holczur@users.noreply.github.com>
2022-10-17 12:56:50 +0200

    chl-object
    I created an object to store two persone's data and write a method to calulate BMI. The method uses 'this' keyword to gain parameters.
    I executed the methods from an if statement to log to the console whose BMI is the higher

--- challenges/chl-script.js
+++ challenges/chl-script.js
@@ -239,3 +239,46 @@ calctip(555);
 calctip(44);
 console.log(tips);
 console.log(total);
+
+/**
+ * Coding Challenge #2-3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+implement the calculations! Remember: BMI = mass / height ** 2 = mass 
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and 
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+method on both objects). Store the BMI value to a property, and also return it 
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the 
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m 
+tall.
+GOOD LUCK 
+ */
+
+const mark = {
+  firstName: 'Mark',
+  lastName: 'Miller',
+  height: 1.69,
+  weight: 78,
+  calcBMI: function(){
+    return this.bmi = this.weight / this.height ** 2;
+  },
+}
+const john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  height: 1.95,
+  weight: 92,
+  calcBMI: function(){
+    return this.bmi = this.weight / this.height ** 2;
+  },
+}
+
+if (mark.calcBMI() > john.calcBMI()) {
+  console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})`)
+} else {
+  console.log(`John's BMI (${john.bmi}) is higher than Mark's (${mark.bmi})`)
+}

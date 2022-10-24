f736db8606ebd6799bc4f42c118e0116d0228f95
holczur <holcz.norbert@gmail.com>
2022-10-24 18:34:14 +0200

    number guessing game
    Start a project, where main focus is on the DOM and how to manipulate it. Problem descreption is ready. RAW implementation is in progress

---
+++ projects/script.js
@@ -0,0 +1,66 @@
+const body = document.querySelector('body');
+const againBtn = document.querySelector('.again');
+const checkBtn = document.querySelector('.check');
+const message = document.querySelector('.message');
+const elScore = document.querySelector('.score');
+const elHighScore = document.querySelector('.highscore');
+const between = document.querySelector('.between');
+const number = document.querySelector('.number');
+const guess = document.querySelector('.guess');
+
+/**
+ A game, where user have to guess a number between 1-20. If She clicks to the 'Check!' button she'll get a message if the number is too low, or too high. If she finds out the number, the backround color will change to green and she gets a message about the answer was correct, also the secret number will be revealed in place of the question mark. Each wrong guess will decrease her score. The score when his guess is correct, will be stored while it is the highscore (or until page is reloaded). Pressing the 'Again!' button will restart the game.
+ 
+ Ingredients:
+ ✔- random number generator
+ 
+ - when Check! is pressed:
+ check if guess == secret number || higher || lower
+ 
+ - when low || high:
+ change message
+ decrease score
+ 
+ - when loose:
+ stop game
+ change message
+ 
+ -when correct
+ stop game
+ change message
+ change backgroud color
+ reveal number
+ set up highscore if needed
+ 
+ - when Again! is pressed:
+ reset message
+ reset score
+ reset background color
+ reset question mark
+        generate new secret number
+        */
+
+const randomGenerator = function () {
+  return Math.floor(Math.random() * 20) + 1;
+};
+
+const secretNumber = randomGenerator();
+console.log(secretNumber);
+let highScore = 0;
+let score = 20;
+
+checkBtn.addEventListener('click', function () {
+  if (guess.value > secretNumber) {
+    message.textContent = '📈 too high';
+    score -= 1;
+    elScore.textContent = score;
+  } else if (guess.value < secretNumber) {
+    message.textContent = '📉 too low';
+    score -= 1;
+    elScore.textContent = score;
+  } else {
+    message.textContent = 'You win';
+    body.style.backgroundColor = '#60b347';
+    number.textContent = secretNumber;
+  }
+});

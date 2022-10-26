5bf8517b60ce449a20070ba5ad264dd93c443678
holczur <115772321+holczur@users.noreply.github.com>
2022-10-26 19:26:31 +0200

    Update
    Winning condition is ready. Continue with again button functionality

--- projects/script.js
+++ projects/script.js
@@ -1,12 +1,12 @@
-const body = document.querySelector('body');
-const againBtn = document.querySelector('.again');
-const checkBtn = document.querySelector('.check');
-const message = document.querySelector('.message');
-const elScore = document.querySelector('.score');
-const elHighScore = document.querySelector('.highscore');
-const between = document.querySelector('.between');
-const number = document.querySelector('.number');
-const guess = document.querySelector('.guess');
+const body = document.querySelector("body");
+const againBtn = document.querySelector(".again");
+const checkBtn = document.querySelector(".check");
+const message = document.querySelector(".message");
+const elScore = document.querySelector(".score");
+const elHighScore = document.querySelector(".highscore");
+const between = document.querySelector(".between");
+const number = document.querySelector(".number");
+const elGuess = document.querySelector(".guess");
 
 /**
  A game, where user have to guess a number between 1-20. If She clicks to the 'Check!' button she'll get a message if the number is too low, or too high. If she finds out the number, the backround color will change to green and she gets a message about the answer was correct, also the secret number will be revealed in place of the question mark. Each wrong guess will decrease her score. The score when his guess is correct, will be stored while it is the highscore (or until page is reloaded). Pressing the 'Again!' button will restart the game.
@@ -45,22 +45,37 @@ const randomGenerator = function () {
 };
 
 const secretNumber = randomGenerator();
-console.log(secretNumber);
 let highScore = 0;
 let score = 20;
 
-checkBtn.addEventListener('click', function () {
-  if (guess.value > secretNumber) {
-    message.textContent = '📈 too high';
-    score -= 1;
+checkBtn.addEventListener("click", function () {
+  let guess = Number(elGuess.value);
+  if (guess > secretNumber) {
+    message.textContent = "📈 too high";
+    score--;
     elScore.textContent = score;
-  } else if (guess.value < secretNumber) {
-    message.textContent = '📉 too low';
-    score -= 1;
+  } else if (guess < secretNumber) {
+    message.textContent = "📉 too low";
+    score--;
     elScore.textContent = score;
   } else {
-    message.textContent = 'You win';
-    body.style.backgroundColor = '#60b347';
+    message.textContent = "You win";
+    body.style.backgroundColor = "#60b347";
     number.textContent = secretNumber;
+    checkBtn.disabled = true;
+    if (score > highScore) {
+      highScore = score;
+      elHighScore.textContent = highScore;
+    }
   }
+});
+
+againBtn.addEventListener("click", function () {
+  message.textContent = "Start guessing...";
+  body.style.backgroundColor = "#222";
+  number.textContent = "?";
+  checkBtn.disabled = false;
+  score = 20;
+  elScore.textContent = score;
+  elGuess.value = "";
 });

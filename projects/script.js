ca114e70c7f0ee26c271d38d0f65ae5736115b16
holczur <115772321+holczur@users.noreply.github.com>
2022-10-30 18:31:34 +0100

    solution updated
    some codes were simplified to increase efficiency

--- projects/script.js
+++ projects/script.js
@@ -1,47 +1,20 @@
+/**
+ A game, where user have to guess a number between 1-20. If She clicks to the 'Check!' button she'll get a message if the number is too low, or too high. If she finds out the number, the backround color will change to green and she gets a message about the answer was correct, also the secret number will be revealed in place of the question mark. Each wrong guess will decrease her score. The score when his guess is correct, will be stored while it is the highscore (or until page is reloaded). Pressing the 'Again!' button will restart the game.
+ */
+
 const body = document.querySelector("body");
 const againBtn = document.querySelector(".again");
 const checkBtn = document.querySelector(".check");
 const message = document.querySelector(".message");
 const elScore = document.querySelector(".score");
 const elHighScore = document.querySelector(".highscore");
-const between = document.querySelector(".between");
 const number = document.querySelector(".number");
 const elGuess = document.querySelector(".guess");
 
-/**
- A game, where user have to guess a number between 1-20. If She clicks to the 'Check!' button she'll get a message if the number is too low, or too high. If she finds out the number, the backround color will change to green and she gets a message about the answer was correct, also the secret number will be revealed in place of the question mark. Each wrong guess will decrease her score. The score when his guess is correct, will be stored while it is the highscore (or until page is reloaded). Pressing the 'Again!' button will restart the game.
- 
- Ingredients:
- ✔- random number generator
- 
- ✔- when Check! is pressed:
- check if guess == secret number || higher || lower
- 
- ✔- when low || high:
- change message
- decrease score
- 
- ✔- when loose:
- stop game
- change message
- 
- ✔-when correct
- stop game
- change message
- change backgroud color
- reveal number
- set up highscore if needed
- 
- ✔- when Again! is pressed:
- reset message
- reset score
- reset background color
- reset question mark
- generate new secret number
- */
-
-const randomGenerator = function () {
-  return Math.floor(Math.random() * 20) + 1;
+const randomGenerator = () => Math.floor(Math.random() * 20) + 1;
+const setScore = () => {
+  score--;
+  elScore.textContent = score;
 };
 
 let secretNumber = randomGenerator();
@@ -50,15 +23,9 @@ let score = 20;
 
 checkBtn.addEventListener("click", function () {
   let guess = Number(elGuess.value);
-  if (guess > secretNumber) {
-    message.textContent = "📈 too high";
-    score--;
-    elScore.textContent = score;
-  } else if (guess < secretNumber) {
-    message.textContent = "📉 too low";
-    score--;
-    elScore.textContent = score;
-  } else {
+  if (guess < 1 || guess > 20) {
+    message.textContent = " Your guess is invalid";
+  } else if (guess === secretNumber) {
     message.textContent = "🥳You win";
     body.style.backgroundColor = "#60b347";
     number.textContent = secretNumber;
@@ -67,13 +34,15 @@ checkBtn.addEventListener("click", function () {
       highScore = score;
       elHighScore.textContent = highScore;
     }
-  }
-  if (score < 1) {
-    message.textContent = "😥You loose";
-    checkBtn.disabled = true;
+  } else {
+    message.textContent = `${
+      guess > secretNumber
+        ? (message.textContent = "📈 too high")
+        : (message.textContent = "📉 too low")
+    }`;
+    setScore();
   }
 });
-
 againBtn.addEventListener("click", function () {
   message.textContent = "Start guessing...";
   body.style.backgroundColor = "#222";

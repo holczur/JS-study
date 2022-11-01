3da1a8218703b997789a99ac892b6d7a9e1d8753
holczur <holcz.norbert@gmail.com>
2022-11-01 18:14:08 +0100

    building up game logic
    Ready: dice rolling, toggle active player, update current and total scores
    
    next: winning condition and game reset

--- projects/pig-game/script.js
+++ projects/pig-game/script.js
@@ -1,2 +1,64 @@
 'use strict';
 
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
+
+let activePlayer = 0;
+let currentScores = [0, 0];
+let scores = [0, 0];
+
+const diceRoll = function () {
+  const diceNum = Math.floor(Math.random() * 6) + 1;
+  dice.src = `./dice-${diceNum}.png`;
+  if (diceNum !== 1) {
+    currentScores[activePlayer] += diceNum;
+    displayCurrent();
+  } else {
+    currentScores[activePlayer] = 0;
+    displayCurrent();
+    toggleActive();
+  }
+};
+
+const toggleActive = function () {
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+};
+
+const hold = function () {
+  scores[activePlayer] += currentScores[activePlayer];
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  currentScores[activePlayer] = 0;
+  displayCurrent();
+  toggleActive();
+};
+
+const displayCurrent = function () {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScores[activePlayer];
+};
+
+btnRoll.addEventListener('click', diceRoll);
+btnHold.addEventListener('click', hold);
+
+/*
+const changePlayer = function () {
+  player1.classList.contains('player--active')
+    ? (p1CurrentScore.textContent = score)
+    : (p2CurrentScore.textContent = score);
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+};
+
+
+btnRoll.addEventListener('click', diceRoll);
+btnHold.addEventListener('click', function () {
+  player1.classList.contains('player--active')
+    ? (elP1Score.textContent = score)
+    : (elP2Score.textContent = score);
+  changePlayer();
+});
+*/

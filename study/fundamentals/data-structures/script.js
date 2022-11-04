bb4d664e86814207829eef128c12554aae18f90d
holczur <115772321+holczur@users.noreply.github.com>
2022-11-04 08:59:52 +0100

    destructuring, spread and rest operator

--- study/fundamentals/data-structures/script.js
+++ study/fundamentals/data-structures/script.js
@@ -14,6 +14,15 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelevery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here's your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
 
   openingHours: {
     thu: {
@@ -56,3 +65,53 @@ const obj = { a: 23, b: 7, c: 14 };
 const {
   fri: { open: o = [], close: c = [] },
 } = openingHours;
+
+restaurant.orderDelevery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const arr = [7, 8, 9];
+const newArr = [1, 2, ...arr];
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//join arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+//... spread operator can be used with all iterables (arrays, strings, maps, sets; NOT objects)
+
+//const ingredients = [prompt('ing1'), prompt('ing2'), prompt('ing3')];
+//restaurant.orderPasta(...ingredients);
+
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giuseppe' };
+console.log(newRestaurant);
+
+//rest ...
+const [one, two, ...others] = [1, 2, 3, 4, 5];
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+const x = [44, 12, 35];
+
+add(2, 3);
+add(7, 8, 12);
+add(2, 2, 5, 7, 345, 776, 8);
+add(...x);

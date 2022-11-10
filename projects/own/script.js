bc877aa410c8dc6b6d2b64c3d9b0b58ee2ebc898
holczur <115772321+holczur@users.noreply.github.com>
2022-11-10 19:03:36 +0100

    scrabble game

---
+++ projects/own/script.js
@@ -0,0 +1,141 @@
+"use strict";
+
+/* ----------------------------- SCRBBLE --------------------------------
+    Table: 15x15
+    letters: 100
+    language: hu
+    max plyers: 4
+
+    Rules:
+        General:
+            - Only valid words, that are appropriate for the language are accepted
+            - The first word has to be in contact with the center square
+            - Words have to be connect to eachother
+            - With each connection all involved words have to be valid
+            - A word can be placed vertical or diagonal only
+            - Only one word can be placed per turn, but that can create multiple other words wth the connections
+        
+        Gameplay:
+            - After a word is placed, the player has to pull as many letters from the sack as she used
+            - Players can pass their turn if they cannot place any word (0 points)
+            - Players can change their letters by pulling from the sack if they want to. If they do so, their turn is over with 0 points.
+            - The game ends if a player had placed all of her letters and the sack is also empty OR
+                All players pass their turn
+            
+        Scores:
+            - Each letter has a point value. A word is worth the sum of teir letters' value.
+            - The first word worth double as its letters' value
+            - There are two blank tiles that can replace any letter, but they have no point values
+            Special areas:
+            - There are five types of special ares that affects the score:
+                Light blue: double letter point
+                Dark blue: triple letter point
+                Yellow: double word score
+                Red: triple word score
+            - Each special area counts only for the first time
+            - If multiple special areas are covered by a word all newly covered areas count in the following order: 1.letter points multipliers 2.word scores multipliers
+            - If a special area is covered for the first time, but with two words, then the area's effect is applied to both letters/words
+            - If a player uses all of his seven letters to create a word she receives +50 bonus scores, that is applied after all bonus area modifiers
+        
+        Final scores:
+            - After the game ends, all players' remaining letters are count as minus from their score.
+            - If the game has ended because a player had placed all of her letters, that player receives all the points the others had lost
+            - The winner is that player, who has the highest score.
+*/
+
+const sack = {
+  a: [6, 1],
+  á: [4, 1],
+  b: [3, 2],
+  c: [1, 5],
+  cs: [1, 7],
+  d: [3, 2],
+  e: [6, 1],
+  é: [3, 3],
+  f: [2, 4],
+  g: [3, 2],
+  gy: [2, 4],
+  h: [2, 3],
+  i: [3, 1],
+  í: [1, 5],
+  j: [2, 4],
+  k: [6, 1],
+  l: [4, 1],
+  ly: [1, 8],
+  m: [3, 1],
+  n: [4, 1],
+  ny: [1, 5],
+  o: [3, 1],
+  ó: [3, 2],
+  ö: [2, 4],
+  ő: [1, 7],
+  p: [2, 4],
+  r: [4, 1],
+  s: [3, 1],
+  sz: [2, 3],
+  t: [5, 1],
+  ty: [1, 10],
+  u: [2, 4],
+  ú: [1, 7],
+  ü: [2, 4],
+  ű: [1, 7],
+  v: [2, 3],
+  z: [2, 4],
+  zs: [1, 8],
+  blank: [2, 0],
+};
+
+//---------------- SACK ------------------
+let sackSize = 100;
+const countSize = function () {
+  sackSize = 0;
+  for (const [num, value] of Object.values(sack)) sackSize += num;
+};
+
+const holder = [];
+const randomPull = function (pieces) {
+  /* 
+    1. húzások száma = funkció paramétere
+2. Minden húzáskor:
+    1. Zsák mérete alapján random szám minden húzáshoz
+    2. a szám alapján találja meg a hozzá tartozó betűt
+    3. a kihúzott betűt tegye az aktív játékos tárhelyére
+    4. csökkentse a zsákban a kihúzott betűhöz tartozó mennyiséget
+    5. ellenőrizze, hogy a mennyiség > 0. Ha <= 0, akkor törölje a kulcsot a zsákból
+    6. csökkentse a zsák méretét eggyel.
+    */
+  const random = function () {
+    return Math.floor(Math.random() * sackSize + 1);
+  };
+  let counter = 0;
+  const temp = [];
+  let index = 0;
+  let key, quantity, value;
+
+  for (let i = 0; i < pieces; i++) {
+    index = random(); //genereate new random number based on new sackSize
+
+    // find random letter
+    for ([key, [quantity, value]] of Object.entries(sack)) {
+      if (counter < index) {
+        counter += quantity;
+        temp.push(key);
+      }
+    }
+
+    // decrease letter quant., if 0, delete key
+    sack[temp.slice(-1)][0]--;
+    if (sack[temp.slice(-1)][0] <= 0) {
+      delete sack[temp.slice(-1)];
+    }
+
+    //push from temp to players holder
+    holder.push(temp.pop());
+    counter = 0;
+    sackSize--;
+    temp.length = 0;
+  }
+
+  console.log(`Holder: ${holder}`);
+};
+//---------------- HOLDER ------------------
